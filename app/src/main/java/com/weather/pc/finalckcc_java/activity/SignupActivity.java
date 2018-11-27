2038eed42297ffc333c8eba0e71519af104b4253
Monysathya <tuy.vanak1@gmail.com>
2018-11-27 20:28:53 +0700

    Add icon to activity_signup

---
+++ app/src/main/java/com/weather/pc/finalckcc_java/activity/SignupActivity.java
@@ -0,0 +1,20 @@
+package com.weather.pc.finalckcc_java.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.weather.pc.finalckcc_java.R;
+
+/**
+ * Created by CKCC on 11/27/2018.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_signup);
+    }
+}
